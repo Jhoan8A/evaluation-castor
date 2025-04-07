90609dc43edf88c84b438ef1b46368fd14344c9f
Jhoan Ochoa Dev <70151766+nvelez-dreamcode@users.noreply.github.com>
2025-04-07 09:23:59 -0500

    new commit

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,2 @@
 INSERT INTO requests(request_date,requester_id, status_id, creation_date) VALUES('2025-03-20','2','3','2025-04-20');
+INSERT INTO requests(request_date,requester_id, status_id, creation_date) VALUES('2025-03-21','2','3','2025-04-21');
